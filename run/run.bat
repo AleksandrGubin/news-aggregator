7a4aeae5408995e48648bb7632909ccb160e6038
gubin <gubin@debian>
2019-08-18 21:39:54 +0500

    Add Readme file and run helper files

---
+++ run/run.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.config.location=.\application.yml news-aggregator-0.0.1-SNAPSHOT.jar
---
+++ run/run.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+java -jar -Dspring.config.location=./application.yml news-aggregator-0.0.1-SNAPSHOT.jar
